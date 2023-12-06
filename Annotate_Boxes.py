de21491b9d5f07f04736a91ebf67acdc66ec124a
Georgios Kontos <kontosgeo04@gmail.com>
2023-12-06 20:15:06 +0100

    Corrected annotations

--- Annotate_Boxes.py
+++ Annotate_Boxes.py
@@ -23,6 +23,13 @@ def show_frame(filepath, no_frame, out):
     cap.release()
     cv2.destroyAllWindows()
 
+def show(filepath):
+    frame = cv2.imread(filepath)
+    plt.connect('button_press_event', on_click)
+    plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    plt.title(filepath)
+    plt.show()
+
 def on_click(event):
     global xs
     global ys
@@ -32,16 +39,16 @@ def on_click(event):
     ys.append(round(event.ydata))
 
 # Script for category I
-count = 1
-path = "dataset/TrainingSet/Categorie I/"
+"""count = 1
+path = "dataset/Frames/Category_I"
 
 for filename in os.scandir(path):
     if filename.is_file():
         ys = []
         xs = []
-        out = 'dataset/Frames/Category_I/plate' + str(count) + '.jpg'
+        # out = 'dataset/Frames/Category_I/plate' + str(count) + '.jpg'
         count += 1
-        show_frame(filename.path, 1, out)
+        show(filename.path)
         max_x = max(xs)
         max_y = max(ys)
         min_x = min(xs)
@@ -50,36 +57,32 @@ for filename in os.scandir(path):
         with open('dataset/bounding_boxes.csv', 'a') as f_object:
             writer_object = writer(f_object)
             writer_object.writerow(l)
-            f_object.close()
+            f_object.close()"""
 
 # Script for category II
 """count = 1
-path = "dataset/TrainingSet/Categorie II"
+path = "dataset/Frames/Category_II"
         
 for filename in os.scandir(path):
     if filename.is_file():
-        no = 1
-        while no < 720:
-            ys = []
-            xs = []
-            out = 'dataset/Frames/Category_II/plate' + str(count) + '.jpg'
-            show_frame(filename.path, no, out)
-            if len(xs) == 4:
-                count += 1
-                max_x = max(xs)
-                max_y = max(ys)
-                min_x = min(xs)
-                min_y = min(ys)
-                l = [filename.name, no, max_y, min_y, min_x, max_y]
-                with open('dataset/bounding_boxes.csv', 'a') as f_object:
-                    writer_object = writer(f_object)
-                    writer_object.writerow(l)
-                    f_object.close()
-            no += 1"""
+        ys = []
+        xs = []
+        #out = 'dataset/Frames/Category_II/plate' + str(count) + '.jpg'
+        count += 1
+        show(filename.path)
+        max_x = max(xs)
+        max_y = max(ys)
+        min_x = min(xs)
+        min_y = min(ys)
+        l = [filename.name, 1, max_y, min_y, min_x, max_x]
+        with open('dataset/bounding_boxes.csv', 'a') as f_object:
+            writer_object = writer(f_object)
+            writer_object.writerow(l)
+            f_object.close()"""
 
 # Script for category III
-"""count = 1
-path = "dataset/TrainingSet/Categorie III"
+count = 1
+path = "dataset/Frames/Category_III"
 
 
 for filename in os.scandir(path):
@@ -88,17 +91,17 @@ for filename in os.scandir(path):
         while c < 2:
             ys = []
             xs = []
-            out = 'dataset/Frames/Category_III/plate' + str(count) + '.jpg'
+            #out = 'dataset/Frames/Category_III/plate' + str(count) + '.jpg'
             if c == 1:
                 count += 1
-            show_frame(filename.path, 4, out)
+            show(filename.path)
             max_x = max(xs)
             max_y = max(ys)
             min_x = min(xs)
             min_y = min(ys)
-            l = [filename.name, 4, max_y, min_y, min_x, max_y]
+            l = [filename.name, 4, max_y, min_y, min_x, max_x]
             with open('dataset/bounding_boxes.csv', 'a') as f_object:
                 writer_object = writer(f_object)
                 writer_object.writerow(l)
                 f_object.close()
-            c += 1"""
+            c += 1
